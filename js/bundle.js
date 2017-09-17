4749f09909ad718f1b4e92c1d552872f31339778
Omayemi Agbeyegebe <grend500@gmail.com>
2017-09-17 11:27:42 -0400

    update bundle

--- js/bundle.js
+++ js/bundle.js
@@ -60,15 +60,102 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Piece {
+  constructor(square, color, board){
+    this.square = square;
+    this.color = color;
+    this.board = board;
+  }
+
+  moves(){
+    return [];
+  }
+
+  validMoves(){
+    const vMoves = [];
+    this.moves().forEach((move)=>{
+      if( !this.board.resultsInCheck(move) &&
+          ( !this.board.pieces[move] ||
+            this.board.pieces[move].color !== this.color)) vMoves.push(move);
+    });
+    return vMoves;
+  }
+
+  rank(){
+    return this.square[1] === 'L' ? this.square[4] : this.square[1];
+  }
+
+  file(){
+    return this.square[1] === 'L' ? this.square[3] : this.square[0];
+  }
+
+  getSquares(offset){
+    const files = 'zabcde';
+    const ranks = '0123456789';
+
+    const sqFile = files.indexOf(this.file()) + offset[0];
+    const sqRank = ranks.indexOf(this.rank()) + offset[1];
+
+    const square = files[sqFile] + ranks[sqRank];
+    //get same square at every level
+    return this.board.existingSquares[square] || [];
+  }
+
+  stepMoves(offsets) {
+    let moves = [];
+    offsets.forEach((offset)=>{
+      moves = moves.concat(this.getSquares(offset));
+    });
+    return moves;
+  }
+
+  slideMoves(directions) {
+    const moves = [];
+
+    directions.forEach((dir)=>{
+      let horizOffset = dir[0];
+      let vertOffset = dir[1];
+
+      let unblocked = true;
+      while(unblocked){
+        const squares = this.getSquares([horizOffset, vertOffset]);
+
+        if(squares.length === 0){
+          unblocked = false;
+          continue;
+        }
+
+        for (let i =0; i < squares.length; i++){
+          if(this.board.pieces[squares[i]]) unblocked = false;
+            moves.push(squares[i]);
+        }
+
+        horizOffset += dir[0];
+        vertOffset += dir[1];
+      }
+    });
+    return moves;
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Piece;
 
 
 
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__board_js__ = __webpack_require__(2);
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -76,11 +163,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const pickPiece = (src, piece, currPos, currOrientation) => {
     boards.greySquare(src, 0xFFFFFF);
     if(selectedSquare ){
+      board.move(selectedSquare + '-' + src);
       boards.move(selectedSquare + '-' + src);
       selectedSquare = null;
       boards.removeGreySquares();
     }
-    else if (piece) selectedSquare= src;
+    else if (piece) {
+      board.pieces[src].validMoves().forEach((sq) => boards.greySquare(sq, 0xFFF000));
+      selectedSquare= src;
+    }
     return false;
   };
   var boards = new ChessBoard3('boards', {
@@ -93,7 +184,299 @@ document.addEventListener('DOMContentLoaded', ()=>{
     notationColor: 0xFFFFFF
   });
 
+  var board = new __WEBPACK_IMPORTED_MODULE_0__board_js__["a" /* default */](boards.position());
+
 });
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__pawn_js__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__king_js__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__queen_js__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__rook_js__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__knight_js__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__bishop_js__ = __webpack_require__(8);
+
+
+
+
+
+
+
+const CONSTANT_SQUARES = {};
+['a','b','c','d'].forEach((file) => {
+  for(let i = 1; i < 13; i++){
+    const square = file +  (i > 3 ? i - Math.floor((i-1)/4) * 2 : i);
+    CONSTANT_SQUARES[square] = CONSTANT_SQUARES[square] || [];
+    CONSTANT_SQUARES[square].push(square + 'WNB'.charAt(Math.floor((i-1)/4)));
+  }
+});
+
+class Board{
+  constructor(position, options = {}){
+    this.pieces = {};
+    this.existingSquares = this.squareList(options.moveableBoards
+                                            || ['QL1','KL1','QL6','KL6']);
+    this.initializeBoard(position);
+    this.lastMoves = [];
+  }
+
+
+
+  initializeBoard(position){
+    Object.keys(position).forEach((square) => {
+      const color = position[square][0];
+      switch (position[square][1]) {
+        case 'P':
+          this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_0__pawn_js__["a" /* default */](square, color, this);
+          break;
+        case 'K':
+          this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_1__king_js__["a" /* default */](square, color, this);
+          break;
+        case 'Q':
+          this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_2__queen_js__["a" /* default */](square, color, this);
+          break;
+        case 'R':
+          this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_3__rook_js__["a" /* default */](square, color, this);
+          break;
+        case 'N':
+          this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_4__knight_js__["a" /* default */](square, color, this);
+          break;
+        case 'B':
+          this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_5__bishop_js__["a" /* default */](square, color, this);
+          break;
+        default:
+
+      }
+    });
+  }
+
+  resultsInCheck(move){
+    return false;
+  }
+  move(move, store = true){
+    if (store) this.lastMoves.push(move);
+    const [startSq, endSq] = [...this.lastMoves.pop().split('-')];
+    if(startSq === endSq) return;
+    this.pieces[endSq] = this.pieces[startSq];
+    this.pieces[endSq].square = endSq;
+    delete this.pieces[startSq];
+  }
+
+  undoLastMove(){
+    if (this.lastMoves.length === 0) return;
+    const [startSq, endSq] = [...this.lastMoves.pop().split('-')];
+    this.move(endSq + '-' + endSq, false);
+  }
+
+  squareList(moveableBoards){
+    const totalSquares = JSON.parse(JSON.stringify(CONSTANT_SQUARES));
+    moveableBoards.forEach((board) => {
+      const loc = board[2] / 1;
+      const ranks = loc % 2 === 0 ? [loc + 2, loc + 3] : [loc - 1, loc];
+      const files = board[0] === 'Q' ? ['z', 'a'] : ['d', 'e'];
+      ranks.forEach((rank) =>{
+        files.forEach((file)=>{
+          totalSquares[file + rank] = totalSquares[file + rank] || [];
+          totalSquares[file + rank].push(board + file + rank);
+        });
+      });
+    });
+    return totalSquares;
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Board;
+
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__piece__ = __webpack_require__(0);
+
+
+class Pawn extends __WEBPACK_IMPORTED_MODULE_0__piece__["a" /* default */] {
+
+  constructor(square, color, board){
+    super(square, color, board);
+    this.homeSquare = square;
+    this.dir = color === 'w' ? 1 : -1;
+  }
+
+  moves(){
+    const allMoves = [];
+    let forwardMoves = this.getSquares([0, this.dir]);
+    if(forwardMoves.length === 0) return [];
+
+    if (this.square === this.homeSquare)
+      forwardMoves = forwardMoves.concat(this.getSquares([0, this.dir * 2]));
+
+    forwardMoves.forEach((sq)=>{
+      if(!this.board.pieces[sq]) allMoves.push(sq);
+    });
+
+    const diagonalMoves = this.getSquares([-1, this.dir]).concat(
+        this.getSquares([1, this.dir])
+    );
+
+    diagonalMoves.forEach((sq) =>{
+      if(this.board.pieces[sq]
+         && this.board.pieces[sq].color !== this.color) allMoves.push(sq);
+    });
+
+    return allMoves;
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Pawn;
+
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__piece__ = __webpack_require__(0);
+
+
+const MOVEOFFSETS = [
+  [ 1, 1],
+  [-1, 1],
+  [ 1,-1],
+  [-1,-1],
+  [ 0, 1],
+  [ 1, 0],
+  [ 0,-1],
+  [-1, 0]
+];
+
+class King extends __WEBPACK_IMPORTED_MODULE_0__piece__["a" /* default */] {
+
+  moves(){
+    return this.stepMoves(MOVEOFFSETS);
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = King;
+
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__piece__ = __webpack_require__(0);
+
+
+const MOVEDIRS = [
+  [1, 1],
+  [-1, 1],
+  [1, -1],
+  [-1, -1],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0]
+];
+
+class Queen extends __WEBPACK_IMPORTED_MODULE_0__piece__["a" /* default */] {
+
+  moves(){
+    return this.slideMoves(MOVEDIRS);
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Queen;
+
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__piece__ = __webpack_require__(0);
+
+
+const MOVEDIRS = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0]
+];
+
+class Rook extends __WEBPACK_IMPORTED_MODULE_0__piece__["a" /* default */] {
+
+  moves(){
+    return this.slideMoves(MOVEDIRS);
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Rook;
+
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__piece_js__ = __webpack_require__(0);
+
+
+const MOVEOFFSETS = [
+  [ 1, -2],
+  [ 1,  2],
+  [-1, -2],
+  [-2, -1],
+  [ 2, -1],
+  [ 2,  1],
+  [-2,  1],
+  [-1,  2]
+];
+
+class Knight extends __WEBPACK_IMPORTED_MODULE_0__piece_js__["a" /* default */] {
+
+  moves(){
+    return this.stepMoves(MOVEOFFSETS);
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Knight;
+
+
+
+/***/ }),
+/* 8 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__piece__ = __webpack_require__(0);
+
+
+const MOVEDIRS = [
+  [1, 1],
+  [-1, 1],
+  [1, -1],
+  [-1, -1]
+];
+
+class Bishop extends __WEBPACK_IMPORTED_MODULE_0__piece__["a" /* default */] {
+
+  moves(){
+    return this.slideMoves(MOVEDIRS);
+  }
+
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Bishop;
+
 
 
 /***/ })
