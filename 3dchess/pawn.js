65efd0621b4c237e619ecfa826a1f6ad8a7fcd7e
Omayemi Agbeyegebe <grend500@gmail.com>
2017-09-17 11:27:22 -0400

    create pawn class and add basic movement and capturing functionality

---
+++ 3dchess/pawn.js
@@ -0,0 +1,34 @@
+import Piece from './piece';
+
+export default class Pawn extends Piece {
+
+  constructor(square, color, board){
+    super(square, color, board);
+    this.homeSquare = square;
+    this.dir = color === 'w' ? 1 : -1;
+  }
+
+  moves(){
+    const allMoves = [];
+    let forwardMoves = this.getSquares([0, this.dir]);
+    if(forwardMoves.length === 0) return [];
+
+    if (this.square === this.homeSquare)
+      forwardMoves = forwardMoves.concat(this.getSquares([0, this.dir * 2]));
+
+    forwardMoves.forEach((sq)=>{
+      if(!this.board.pieces[sq]) allMoves.push(sq);
+    });
+
+    const diagonalMoves = this.getSquares([-1, this.dir]).concat(
+        this.getSquares([1, this.dir])
+    );
+
+    diagonalMoves.forEach((sq) =>{
+      if(this.board.pieces[sq]
+         && this.board.pieces[sq].color !== this.color) allMoves.push(sq);
+    });
+
+    return allMoves;
+  }
+}
