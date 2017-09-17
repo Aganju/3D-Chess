3df2470525b1051372298c993c0e0f698653c023
Omayemi Agbeyegebe <grend500@gmail.com>
2017-09-17 07:42:03 -0400

    use logical board representation to find piece  moves

--- 3dchess/game.js
+++ 3dchess/game.js
@@ -1,13 +1,19 @@
+import Board from './board.js';
+
 document.addEventListener('DOMContentLoaded', ()=>{
   let selectedSquare;
   const pickPiece = (src, piece, currPos, currOrientation) => {
     boards.greySquare(src, 0xFFFFFF);
     if(selectedSquare ){
+      board.move(selectedSquare + '-' + src);
       boards.move(selectedSquare + '-' + src);
       selectedSquare = null;
       boards.removeGreySquares();
     }
-    else if (piece) selectedSquare= src;
+    else if (piece) {
+      board.pieces[src].validMoves().forEach((sq) => boards.greySquare(sq, 0xFFF000));
+      selectedSquare= src;
+    }
     return false;
   };
   var boards = new ChessBoard3('boards', {
@@ -19,5 +25,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     backgroundColor: 0x000000,
     notationColor: 0xFFFFFF
   });
+
+  var board = new Board(boards.position());
 
 });
