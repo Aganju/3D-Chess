a25262ea7f9185644c798933691ce7c103503ebb
Omayemi Agbeyegebe <grend500@gmail.com>
2017-09-22 11:25:31 -0400

    update bundle

--- js/bundle.js
+++ js/bundle.js
@@ -81,10 +81,10 @@ class Piece {
 
   validMoves(){
     const vMoves = [];
-    this.moves().forEach((move)=>{
-      if( !this.board.resultsInCheck(move) &&
-          ( !this.board.pieces[move] ||
-            this.board.pieces[move].color !== this.color)) vMoves.push(move);
+    this.moves().forEach((moveSquare)=>{
+      if( ( !this.board.pieces[moveSquare] || this.board.pieces[moveSquare].color !== this.color)
+          && !this.board.resultsInCheck(this.square + '-' + moveSquare))
+          vMoves.push(moveSquare);
     });
     return vMoves;
   }
@@ -163,10 +163,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const pickPiece = (src, piece, currPos, currOrientation) => {
     boards.greySquare(src, 0xFFFFFF);
     if(selectedSquare ){
-      board.move(selectedSquare + '-' + src);
-      boards.move(selectedSquare + '-' + src);
-      selectedSquare = null;
       boards.removeGreySquares();
+      if(src != selectedSquare){
+        board.move(selectedSquare + '-' + src);
+        boards.move(selectedSquare + '-' + src);
+      }
+      selectedSquare = null;
     }
     else if (piece) {
       board.pieces[src].validMoves().forEach((sq) => boards.greySquare(sq, 0xFFF000));
@@ -221,6 +223,7 @@ class Board{
     this.pieces = {};
     this.existingSquares = this.squareList(options.moveableBoards
                                             || ['QL1','KL1','QL6','KL6']);
+    this.kings = {};
     this.initializeBoard(position);
     this.lastMoves = [];
   }
@@ -235,7 +238,9 @@ class Board{
           this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_0__pawn_js__["a" /* default */](square, color, this);
           break;
         case 'K':
+        //keep reference to kings to save time on move into check validation
           this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_1__king_js__["a" /* default */](square, color, this);
+          this.kings[color] = this.pieces[square];
           break;
         case 'Q':
           this.pieces[square] = new __WEBPACK_IMPORTED_MODULE_2__queen_js__["a" /* default */](square, color, this);
@@ -256,12 +261,78 @@ class Board{
   }
 
   resultsInCheck(move){
+    const pieceColor = this.pieces[move.split('-')[0]].color;
+    const king = this.kings[pieceColor];
+
+    const straightOffsets = [ [0, 1], [1, 0], [0, -1], [-1, 0]  ];
+    const diagOffsets = [ [1, 1], [-1, 1], [1, -1], [-1, -1] ];
+    const knightOffsets = [ [ 1, -2], [ 1,  2], [-1, -2],  [-2, -1],
+                            [ 2, -1], [ 2,  1], [-2,  1],  [-1,  2] ];
+    const pawnDir = king.color === 'w' ? 1 : -1;
+    const stepOffsets = [ [1, pawnDir], [-1, pawnDir] ].concat(knightOffsets);
+
+    const checkForPiece = (squares, piece) => {
+      if(squares.length !== 0){
+          const lastSquare = squares[squares.length -1];
+          const lastSquares = this.existingSquares[lastSquare[1] === 'L' ?
+              lastSquare.substr(3, 5) : lastSquare.substr(0, 2)];
+
+          for(let j = 0; j < lastSquares.length; j++){
+            const possPiece = this.pieces[lastSquares[j]];
+            if(possPiece && possPiece.color !== king.color){
+              const pieceType = possPiece.constructor.name;
+              if(pieceType === piece ||
+                  ( (piece === 'Rook' || piece === 'Bishop')
+                  && pieceType === 'Queen' )
+               ) return true;
+            }
+
+          }
+      }
+      return false;
+    };
+
+    this.move(move);
+
+    for(let i = 0; i < 4; i++){
+      const straightMoves = king.slideMoves([straightOffsets[i]]);
+      const diagMoves = king.slideMoves([diagOffsets[i]]);
+
+      if(checkForPiece(straightMoves, 'Rook') || checkForPiece(diagMoves, 'Bishop')){
+        this.undoLastMove();
+        return true;
+      }
+
+    }
+
+    for(let i = 0; i < stepOffsets.length; i++){
+      const piece = i < 2 ? 'Pawn' : 'Knight';
+      if (checkForPiece(king.getSquares(stepOffsets[i]), piece)){
+        this.undoLastMove();
+        return true;
+      }
+    }
+
+    //if it's the kings move then his moveset might include the other king
+    if(this.pieces[move.split('-')[1]] === king){
+      const kingMoves = king.moves();
+      for(let i = 0; i < kingMoves.length; i++){
+        const piece = this.pieces[kingMoves[i]];
+        if(piece && piece.constructor.name === 'King'){
+          this.undoLastMove();
+          return true;
+        }
+      }
+    }
+
+    this.undoLastMove();
     return false;
+
   }
+
   move(move, store = true){
-    if (store) this.lastMoves.push(move);
-    const [startSq, endSq] = [...this.lastMoves.pop().split('-')];
-    if(startSq === endSq) return;
+    const [startSq, endSq] = [...move.split('-')];
+    if (store) this.lastMoves.push([move, this.pieces[endSq]]);
     this.pieces[endSq] = this.pieces[startSq];
     this.pieces[endSq].square = endSq;
     delete this.pieces[startSq];
@@ -269,8 +340,10 @@ class Board{
 
   undoLastMove(){
     if (this.lastMoves.length === 0) return;
-    const [startSq, endSq] = [...this.lastMoves.pop().split('-')];
-    this.move(endSq + '-' + endSq, false);
+    const [move, piece] = [...this.lastMoves.pop()];
+    const [startSq, endSq] = move.split('-');
+    this.move(endSq + '-' + startSq, false);
+    if(piece) this.pieces[endSq] = piece;
   }
 
   squareList(moveableBoards){
