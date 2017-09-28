109cae8fd49b59f483d89a941bcbe37a9c247f31
Omayemi Agbeyegebe <grend500@gmail.com>
2017-09-27 21:17:00 -0400

    refactor game to allow for different modes and settings plus add ai player

--- 3dchess/game.js
+++ 3dchess/game.js
@@ -1,33 +1,108 @@
 import Board from './board.js';
 
 document.addEventListener('DOMContentLoaded', ()=>{
-  let selectedSquare;
-  const pickPiece = (src, piece, currPos, currOrientation) => {
-    boards.greySquare(src, 0xFFFFFF);
-    if(selectedSquare ){
-      boards.removeGreySquares();
-      if(src != selectedSquare){
-        board.move(selectedSquare + '-' + src);
-        boards.move(selectedSquare + '-' + src);
+  new Game('boards', undefined, 'continueGame');
+});
+
+
+class Game{
+  constructor(container, position = 'start', gameType = 'freePlace'){
+    this.container = container;
+    this.visualBoard = this.initializeVisualBoard(position);
+    this.logicBoard = new Board(this.visualBoard.position());
+    this.moveHandler = this[gameType];
+    this.draggable = false;
+    this.players = {w: 'human', b: 'AI'};
+    this.currentPlayer = 'w';
+  }
+
+  initializeVisualBoard(position){
+    return new ChessBoard3(this.container, {
+      draggable: true,
+      position: position,
+      onDragStart: this.handleBoardClick.bind(this),
+      showErrors: 'console',
+      zoomControls: true,
+      backgroundColor: 0x000000,
+      notationColor: 0xFFFFFF
+    });
+  }
+
+  selectSquare(square){
+    this.visualBoard.greySquare(square, 0xFFFFFF);
+    this.validMoves = this.logicBoard.pieces[square].validMoves();
+    this.validMoves.forEach(
+      (sq) => this.visualBoard.greySquare(sq, 0xFFF000));
+    this.selectedSquare= square;
+  }
+
+  makeMove(square){
+    this.visualBoard.removeGreySquares();
+    if(square !== this.selectedSquare){
+      this.logicBoard.move(this.selectedSquare + '-' + square);
+      this.visualBoard.move(this.selectedSquare + '-' + square);
+    }
+    this.selectedSquare = null;
+
+  }
+
+  continueGame(square, piece){
+    if (this.selectedSquare ){
+      const cancelMove = (square === this.selectedSquare);
+      let validMove = cancelMove;
+      let i = 0;
+      while (!validMove && i < this.validMoves.length) {
+        if(this.validMoves[i] === square) validMove = true;
+        i+=1;
       }
-      selectedSquare = null;
+      if(validMove ){
+        this.makeMove(square);
+        if(!cancelMove)this.flipPlayer();
+      }
+    }//check if piece is seleted and color matches
+    else if( piece && piece[0] === this.currentPlayer){
+      this.selectSquare(square);
+    }
+  }
+
+  aiPlayer(){
+    const pieces = Object.values(this.logicBoard.pieces);
+    let validMoves, piece = pieces[Math.round(Math.random()*(pieces.length-1))];
+    while(piece.color !== this.currentPlayer ||
+          (validMoves = piece.validMoves()).length === 0){
+      piece = pieces[Math.round(Math.random()*pieces.length-1)];
+    }
+    this.selectSquare(piece.square);
+    this.makeMove(validMoves[Math.round(Math.random()*(validMoves.length-1))]);
+  }
+
+  freePlace(square, piece){
+    this.visualBoard.greySquare(square, 0xFFFFFF);
+    if(this.selectedSquare ){
+      this.makeMove(square);
     }
     else if (piece) {
-      board.pieces[src].validMoves().forEach((sq) => boards.greySquare(sq, 0xFFF000));
-      selectedSquare= src;
+      this.selectSquare();
     }
-    return false;
-  };
-  var boards = new ChessBoard3('boards', {
-    draggable: true,
-    position: 'start',
-    onDragStart: pickPiece,
-    showErrors: 'console',
-    zoomControls: true,
-    backgroundColor: 0x000000,
-    notationColor: 0xFFFFFF
-  });
 
-  var board = new Board(boards.position());
+  }
 
-});
+  flipPlayer(){
+    this.currentPlayer = this.currentPlayer === 'w' ? 'b' : 'w';
+    if(this.players[this.currentPlayer] === 'AI'){
+      setTimeout(() => {
+        this.aiPlayer();
+        this.flipPlayer();
+      }, 1000);
+    }
+  }
+  switchMode(mode){
+
+  }
+
+  handleBoardClick(square, piece, currPos, currOrientation){
+    this.moveHandler(square, piece);
+    return this.draggable;
+  }
+
+}
